b3e79b7239b118458fb8d83737876013475f51f7
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-12-01 14:38:37 -0400

    CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const { dbConnection } = require("./database/config");
 
 //console.log(process.env);
@@ -9,6 +10,9 @@ const app = express();
 
 //Base de datos
 dbConnection();
+
+//CORS
+app.use(cors());
 
 //directorio publico
 app.use(express.static("public"));
